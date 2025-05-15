3cec99f49e9cfc71b43f1fa7881e7fb8bb81bc52
campo <campo@MAXIS-BOOK>
2025-05-15 11:09:39 -0400

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,35 +1,44 @@
 <template>
   <nav>
-    <a href="/" class="blue">[main.py]</a>
-    <a href="/education" class="green">[educacion.txt]</a>
-    <a href="/cursos" class="blue">[cursos.json]</a>
-    <a href="/experience" class="green">[experiencia.sql]</a>
-    <a href="/skills" class="blue">[skills.conf]</a>
+    <router-link to="/" class="blue" exact>[main.py]</router-link>
+    <router-link to="/education" class="green">[educacion.txt]</router-link>
+    <router-link to="/cursos" class="blue">[cursos.json]</router-link>
+    <router-link to="/experience" class="green">[experiencia.sql]</router-link>
+    <router-link to="/skills" class="blue">[skills.conf]</router-link>
   </nav>
 </template>
 
 <style scoped>
 a {
   margin: 1vw;
-}
-.blue {
-  color: blue;
   text-decoration: none;
 }
+
+.blue {
+  color: blue;
+}
+
 .blue:hover {
   color: white;
   background-color: blue;
 }
+
 .green {
   color: #33ff00;
-  text-decoration: none;
 }
+
 .green:hover {
   color: black;
   background-color: #33ff00;
 }
+
 nav {
   margin-bottom: 2vh;
   margin-top: 1vh;
+}
+
+.router-link-active {
+  font-weight: bold;
+  text-decoration: underline;
 }
 </style>
